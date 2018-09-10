48518a1f409a9159c1b101d34f89cc7b8fd59596
Administrator <Administrator@MININT-F36S5EH>
2018-09-09 18:41:32 -0700

    initial commit

---
+++ AAX_QueueTestConsoleApp/Program.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.ServiceBus.Messaging;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Json;
+
+
+namespace AAX_QueueTestConsoleApp
+{
+    class Program
+    {
+        static void sendMessage()
+        {
+            string connectionString = "Endpoint=sb://aaxtest1.servicebus.windows.net/;SharedAccessKeyName=AAXTEST1SharedAccess;SharedAccessKey=tqPyCRjcMk5pVzpfFO81aJz8LFqGbWV542ayJCO6GNM=;EntityPath=aaxtest1queue";
+            QueueClient queueClient;
+            string messageBody = "Test Message " + System.Guid.NewGuid();
+ 
+            BrokeredMessage message = new BrokeredMessage(messageBody);
+
+            queueClient = QueueClient.CreateFromConnectionString(connectionString, ReceiveMode.PeekLock);
+
+            String s = message.GetBody<String>();
+
+            queueClient.Send(message);
+
+            queueClient.Close();
+
+            Console.WriteLine("message written: " + s );
+        }
+
+        static void receiveMessage()
+        {
+            string connectionString = "Endpoint=sb://aaxtest1.servicebus.windows.net/;SharedAccessKeyName=AAXTEST1SharedAccess;SharedAccessKey=tqPyCRjcMk5pVzpfFO81aJz8LFqGbWV542ayJCO6GNM=;EntityPath=aaxtest1queue";
+            QueueClient queueClient;
+            BrokeredMessage message;
+
+            queueClient = QueueClient.CreateFromConnectionString(connectionString, ReceiveMode.PeekLock);
+
+            message = queueClient.Receive();
+
+            string s = message.GetBody<String>();
+
+            queueClient.Complete(message.LockToken);
+
+            queueClient.Close();
+
+            Console.WriteLine("received message: " + s);
+        }
+        static void Main(string[] args)
+        {
+            sendMessage();
+            receiveMessage();
+
+            Console.ReadLine();
+        }
+    }
+}
